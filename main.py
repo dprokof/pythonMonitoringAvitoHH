e2122ec2282a33a82a1791a633573cde1735cdf4
Haspro68 <has.pro@yandex.ru>
2024-09-13 15:30:51 +0400

    Add region and search debug

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 import gspread
 import datetime
 from multiprocessing import Pool
-from selenium.common import NoSuchElementException, TimeoutException
-from selenium.webdriver.chrome.webdriver import WebDriver as Chromedriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,11 +14,12 @@ from selenium.webdriver.support import expected_conditions as EC
 def create_driver():
     options = ChromeOptions()
     # options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    # options.add_argument('--disable-extensions')
-    options.add_argument("--user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0")
+    options.add_argument('--disable-extensions')
+    options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, например Gecko) Chrome/45.0.2454.85 Safari/537.36")
     options.add_argument('--no-sandbox')
-    driver = Chromedriver(options=options)
+    driver = ChromeDriver(options=options)
 
     return driver
 
@@ -28,17 +29,12 @@ def validator_url_hh(url):
         url = re.sub(r'\?.*', "?host=hh.ru", url)
 
     elif 'adsrv.hh.ru' in url:
-        try:
-            driver = create_driver()
-            driver.implicitly_wait(3)
-            driver.get(url)
-            current_url = driver.current_url
-            url = re.sub(r'\?.*', "?host=hh.ru", current_url)
-        except TimeoutException:
-            pass
-        finally:
-            driver.close()
-            driver.quit()
+        driver = create_driver()
+        driver.get(url)
+        time.sleep(3)
+        current_url = driver.current_url
+        url = re.sub(r'\?.*', "?host=hh.ru", current_url)
+        driver.close()
 
     elif 'from=employer' in url:
         url = re.sub(r'\?.*', "?host=hh.ru", url)
@@ -62,12 +58,12 @@ def get_pages_count_hh(vacancy):
 def search_vacancy_hh(vacancy_url):
     data = []
     vacancy_url = validator_url_hh(vacancy_url)
-    driver = create_driver()
-    driver.get(f'https://hh.ru/search/vacancy?text={vacancy}&area=1')
     flag = False
     current_page = 1
     position = 1
     try:
+        driver = create_driver()
+        driver.get(f'https://hh.ru/search/vacancy?text={vacancy}&area=1')
         while True:
             vacancy_cards = driver.find_elements(by=By.CLASS_NAME, value='vacancy-card--z_UXteNo7bRGzxWVcL7y')
             for vacancy_card in vacancy_cards:
@@ -83,8 +79,7 @@ def search_vacancy_hh(vacancy_url):
             if flag:
                 break
             try:
-                wait = WebDriverWait(driver, 10)
-                next = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-qa="number-pages-next"]')))
+                next = driver.find_element(by=By.CSS_SELECTOR, value='[data-qa="number-pages-next"]')
                 driver.execute_script("arguments[0].click();", next)
                 current_page += 1
             except NoSuchElementException as ex:
@@ -92,6 +87,10 @@ def search_vacancy_hh(vacancy_url):
                 current_position = 'Не найдено'
                 current_page = 'Не найдено'
                 break
+
+            finally:
+                driver.close()
+                driver.quit()
 
         current_date = datetime.datetime.now().strftime('%d-%m-%Y')
         dt = datetime.datetime.now()
@@ -102,13 +101,17 @@ def search_vacancy_hh(vacancy_url):
         data.append(vacancy_url)
         data.append(current_page)
         data.append(current_position)
-        wks = gc.open("Вакансии").sheet1
+        wks = gc.open("Test_Parsing_HH_and_Avitro").sheet1
         wks.append_row(data)
         driver.close()
         driver.quit()
 
     except Exception as ex:
         print(ex)
+
+    finally:
+        driver.close()
+        driver.quit()
 
 
 def search_vacancy_avito(vacancy_url):
@@ -146,6 +149,10 @@ def search_vacancy_avito(vacancy_url):
             current_page = 'Не найдено'
             break
 
+        finally:
+            driver.close()
+            driver.quit()
+
     current_date = datetime.datetime.now().strftime('%d-%m-%Y')
     dt = datetime.datetime.now()
     current_time = f"{dt.hour}:{dt.minute}"
@@ -155,14 +162,14 @@ def search_vacancy_avito(vacancy_url):
     data.append(vacancy_url)
     data.append(current_page)
     data.append(current_position)
-    wks = gc.open("Вакансии").get_worksheet(1)
+    wks = gc.open("Test_Parsing_HH_and_Avitro").get_worksheet(1)
     wks.append_row(data)
     driver.close()
     driver.quit()
 
 
 if __name__ == "__main__":
-    gc = gspread.service_account(filename='../pythonMonitoringAvitoHH/key.json')
+    gc = gspread.service_account(filename='api_key.json')
     print('Выберите платформу для мониторинга\n'
           '\n'
           'Введите 1 - HH.ru\n'
