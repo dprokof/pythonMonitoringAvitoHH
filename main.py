c5cdef193b1e8a3add43daac119323c12b7956e7
Haspro68 <has.pro@yandex.ru>
2024-09-13 15:39:45 +0400

    Add region and search debug

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import time
 import gspread
 import datetime
 from multiprocessing import Pool
-from selenium.common import NoSuchElementException
-from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
+
+from selenium.common import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.webdriver import WebDriver as Chromedriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,12 +15,11 @@ from selenium.webdriver.support import expected_conditions as EC
 def create_driver():
     options = ChromeOptions()
     # options.add_argument('--headless')
-    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
-    options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, например Gecko) Chrome/45.0.2454.85 Safari/537.36")
+    # options.add_argument('--disable-extensions')
+    options.add_argument("--user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0")
     options.add_argument('--no-sandbox')
-    driver = ChromeDriver(options=options)
+    driver = Chromedriver(options=options)
 
     return driver
 
@@ -29,12 +29,17 @@ def validator_url_hh(url):
         url = re.sub(r'\?.*', "?host=hh.ru", url)
 
     elif 'adsrv.hh.ru' in url:
-        driver = create_driver()
-        driver.get(url)
-        time.sleep(3)
-        current_url = driver.current_url
-        url = re.sub(r'\?.*', "?host=hh.ru", current_url)
-        driver.close()
+        try:
+            driver = create_driver()
+            driver.implicitly_wait(3)
+            driver.get(url)
+            current_url = driver.current_url
+            url = re.sub(r'\?.*', "?host=hh.ru", current_url)
+        except TimeoutException:
+            pass
+        finally:
+            driver.close()
+            driver.quit()
 
     elif 'from=employer' in url:
         url = re.sub(r'\?.*', "?host=hh.ru", url)
@@ -56,68 +61,55 @@ def get_pages_count_hh(vacancy):
 
 
 def search_vacancy_hh(vacancy_url):
-    data = []
     vacancy_url = validator_url_hh(vacancy_url)
+    driver = create_driver()
+    pages = get_pages_count_hh(vacancy)
+    data = []
     flag = False
-    current_page = 1
-    position = 1
-    try:
-        driver = create_driver()
-        driver.get(f'https://hh.ru/search/vacancy?text={vacancy}&area=1')
-        while True:
-            vacancy_cards = driver.find_elements(by=By.CLASS_NAME, value='vacancy-card--z_UXteNo7bRGzxWVcL7y')
-            for vacancy_card in vacancy_cards:
-                position += 1
-                url = vacancy_card.find_element(by=By.TAG_NAME, value='a').get_attribute('href')
-                url = validator_url_hh(url)
-                if url == vacancy_url:
-                    flag = True
-                    current_position = position
-                    break
-            position = 1
-
-            if flag:
+    for page in range(0, pages + 1):
+        driver.get(f'https://hh.ru/search/vacancy?text={vacancy}&area={area}&page={page}')
+        vacancy_cards = driver.find_elements(by=By.CLASS_NAME, value='vacancy-info--umZA61PpMY07JVJtomBA')
+        current_position = 1
+        for vacancy_card in vacancy_cards:
+            url = vacancy_card.find_element(by=By.TAG_NAME, value='a').get_attribute('href')
+            url = validator_url_hh(url)
+            if url == vacancy_url:
+                current_page = page
+                flag = True
                 break
-            try:
-                next = driver.find_element(by=By.CSS_SELECTOR, value='[data-qa="number-pages-next"]')
-                driver.execute_script("arguments[0].click();", next)
-                current_page += 1
-            except NoSuchElementException as ex:
-                print(ex)
-                current_position = 'Не найдено'
-                current_page = 'Не найдено'
-                break
+            current_position += 1
 
-            finally:
-                driver.close()
-                driver.quit()
+        if flag:
+            current_page = current_page + 1
+            if current_page == 41:
+                current_page = 40
+            current_date = datetime.datetime.now().strftime('%d-%m-%Y')
+            dt = datetime.datetime.now()
+            current_time = f"{dt.hour}:{dt.minute}"
+            data.append(current_date)
+            data.append(current_time)
+            if area == 1:
+                data.apeend('Мск')
+            else:
+                data.append('Спб')
+            data.append(vacancy)
+            data.append(vacancy_url)
+            data.append(current_page)
+            data.append(current_position)
+            wks = gc.open("Вакансии").sheet1
+            wks.append_row(data)
+            driver.close()
+            driver.quit()
+            return
 
-        current_date = datetime.datetime.now().strftime('%d-%m-%Y')
-        dt = datetime.datetime.now()
-        current_time = f"{dt.hour}:{dt.minute}"
-        data.append(current_date)
-        data.append(current_time)
-        data.append(vacancy)
-        data.append(vacancy_url)
-        data.append(current_page)
-        data.append(current_position)
-        wks = gc.open("Test_Parsing_HH_and_Avitro").sheet1
-        wks.append_row(data)
-        driver.close()
-        driver.quit()
-
-    except Exception as ex:
-        print(ex)
-
-    finally:
-        driver.close()
-        driver.quit()
+    driver.close()
+    driver.quit()
 
 
 def search_vacancy_avito(vacancy_url):
     data = []
     driver = create_driver()
-    driver.get(f"https://www.avito.ru/moskva/vakansii?q={vacancy}")
+    driver.get(f"https://www.avito.ru/{area}/vakansii?q={vacancy}")
     position = 1
     current_page = 1
     flag = False
@@ -149,33 +141,42 @@ def search_vacancy_avito(vacancy_url):
             current_page = 'Не найдено'
             break
 
-        finally:
-            driver.close()
-            driver.quit()
+        except TimeoutException:
+            current_position = 'Не найдено'
+            current_page = 'Не найдено'
+            break
 
     current_date = datetime.datetime.now().strftime('%d-%m-%Y')
     dt = datetime.datetime.now()
     current_time = f"{dt.hour}:{dt.minute}"
     data.append(current_date)
     data.append(current_time)
+    if area == 'moskva':
+        data.append('Мск')
+    else:
+        data.append('Спб')
     data.append(vacancy)
     data.append(vacancy_url)
     data.append(current_page)
     data.append(current_position)
-    wks = gc.open("Test_Parsing_HH_and_Avitro").get_worksheet(1)
+    wks = gc.open("Вакансии").get_worksheet(1)
     wks.append_row(data)
     driver.close()
     driver.quit()
 
 
 if __name__ == "__main__":
-    gc = gspread.service_account(filename='api_key.json')
+    gc = gspread.service_account(filename='../pythonMonitoringAvitoHH/key.json')
     print('Выберите платформу для мониторинга\n'
           '\n'
           'Введите 1 - HH.ru\n'
           'Введите 2 - Avito\n')
     choice = int(input('Введите: '))
     if choice == 1:
+        area = int(input('Выберите регион поиска\n'
+                        '1 - Москва\n'
+                        '2 - Санкт-Петербург\n'
+                        'Введите: '))
         vacancy = input('Введите название вакансии: ')
         print()
         vacancy_url = input('Введите ссылку на вакансию\n'
@@ -190,6 +191,14 @@ if __name__ == "__main__":
 
 
     else:
+        area = int(input('Выберите регион поиска\n'
+                        '1 - Москва\n'
+                        '2 - Санкт-Петербург\n'
+                        'Введите: '))
+        if area == 1:
+            area = 'moskva'
+        else:
+            area = 'sankt-peterburg'
         vacancy = input('Введите название вакансии: ')
         print()
         vacancy_url = input('Введите ссылку на вакансию\n'
